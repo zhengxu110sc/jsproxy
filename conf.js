29a92e731f4397f266aa699a8af5ecfdd29d22ca
郑旭的个人主页 <38687093+zhengxu110sc@users.noreply.github.com>
2019-11-25 16:28:11 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -43,7 +43,7 @@ jsproxy_config({
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        'b.007.workers.dev': 1,
+        'https://raspy-snow-1d45.zhengxu.workers.dev': 1,
         'b.hehe.workers.dev': 1,
         'b.lulu.workers.dev': 1,
         'b.jsproxy.workers.dev': 1,
